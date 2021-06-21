d461536d647d4587c7a27fb7c528b7c6eb71d897
Vitaliy Petelchuk <v000dev@gmail.com>
2021-06-21 23:43:12 +0300

    ready

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">max-mebel</h1>
+      <h1 class="title">Можно начинать все работает</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
